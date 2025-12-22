c745201701389b1a8dbd0abd26fc6cfe20f7cec6
Satis9815 <satiskumarchaudhary52@gmail.com>
2025-12-22 22:11:07 +0545

    solved day 5 and day 6 remaining questions

--- Day_05/script.js
+++ Day_05/script.js
@@ -3,20 +3,26 @@
 // Output: [1, 2, 3, 4, 6, 8, 12, 24]
 function getAllFactors(num) {
   let factors = [];
-  for (let i = 1; i <= num; i++) {
+  let loopCount = 0;
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    loopCount++;
     if (num % i === 0) {
-      factors.push(i);
+      factors.splice(factors.length / 2, 0, i);
+      let otherPair = num / i;
+      if (i !== otherPair) factors.splice(factors.length / 2 + 1, 0, otherPair);
     }
   }
   return factors;
 }
 
 // console.log(getAllFactors(10));
+// console.log(getAllFactors(27));
+// console.log(getAllFactors(100));
 
 // 2.Print All Multiples of a Number up to N In Array Format
 // Input: Number = 4, Limit = 30
 // Output: [4, 8, 12, 16, 20, 24, 28]
-function multiplesOfANumner(num, limit) {
+function multiplesOfANumber(num, limit) {
   let multiples = [];
   for (let i = num; i <= limit; i += num) {
     if (i % num === 0) {
@@ -25,7 +31,7 @@ function multiplesOfANumner(num, limit) {
   }
   return multiples;
 }
-// console.log(multiplesOfANumner(2, 12));
+// console.log(multiplesOfANumber(2, 12));
 
 // 3.Find the HCF (Highest Common Factor) or GCD of Two Numbers
 // Input: 12, 18
@@ -33,25 +39,43 @@ function multiplesOfANumner(num, limit) {
 //1,2,3,6,9,18
 // Output: HCF = 6
 function HCF(num1, num2) {
-  let factors1 = [];
+  let factors = [];
+  let loopCount = 0;
   let greatestCF;
-  for (let i = 1; i <= num1; i++) {
-    if (num1 % i === 0) {
-      factors1.push(i);
-    }
+  let divisor = num1 < num2 ? num1 : num2;
+  let dividend = num1 > num2 ? num1 : num2;
+  while (dividend % divisor !== 0) {
+    loopCount++;
+    const remainder = dividend % divisor;
+    dividend = divisor;
+    divisor = remainder;
   }
+  console.log({ loopCount });
+  return divisor;
 
-  for (let i = factors1.length; i > 0; i--) {
-    // debugger
-    if (num2 % factors1[i] === 0) {
-      greatestCF = factors1[i];
-      break;
-    }
-  }
+  // Another way
+  // for (let i = 1; i <= Math.sqrt(num1); i++) {
+  //   loopCount++;
+  //   if (num1 % i === 0) {
+  //     factors.splice(factors.length / 2, 0, i);
+  //     let otherPair = num1 / i;
+  //     if (i !== otherPair) factors.splice(factors.length / 2 + 1, 0, otherPair);
+  //   }
+  // }
 
-  return { greatestCF };
+  // for (let i = factors.length; i > 0; i--) {
+  //   if (num2 % factors[i] === 0) {
+  //     greatestCF = factors[i];
+  //     break;
+  //   }
+  // }
+
+  // return { greatestCF,loopCount };
 }
-console.log(HCF(25, 15));
+
+// console.log(HCF(25, 45));
+// console.log(HCF(18, 12));
+// console.log(HCF(8, 10));
 
 // 4. Find the LCM (Least Common Multiple) of Two Numbers
 function findLCM(a, b) {
@@ -89,3 +113,90 @@ function sumOfAllFactors(num) {
   return sum;
 }
 // console.log(sumOfAllFactors(12));
+
+// 5.Count the Total Number of Factors of a Number
+// Input: N = 24
+// Output: 8 Factors
+function totalFactors(num) {
+  let factors = [];
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      factors.splice(factors.length / 2, 0, i);
+      let otherPair = num / i;
+      if (i !== otherPair) factors.splice(factors.length / 2 + 1, 0, otherPair);
+    }
+  }
+  return factors.length;
+}
+// console.log(totalFactors(24));
+
+// 6.Sum of All Factors of a Number
+// Input: N = 12
+// Output: Sum = 28
+
+function sumOfAllFactors(num) {
+  let factors = [];
+  let sum = 0;
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      factors.splice(factors.length / 2, 0, i);
+      let otherPair = num / i;
+      if (i !== otherPair) factors.splice(factors.length / 2 + 1, 0, otherPair);
+    }
+  }
+  for (let i = 0; i < factors.length; i++) {
+    sum = sum + factors[i];
+  }
+  return sum;
+}
+// console.log(sumOfAllFactors(12));
+
+// 7.Find the Greatest Factor of a Number (Other Than Itself)
+// Input: N = 36
+// Output: 18
+function greatestFactor(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (i % 2 === 0) {
+      return num / i;
+    }
+    return 1;
+  }
+}
+// console.log(greatestFactor(10));
+
+// 8.Check if a Number is a Perfect Number
+// Input: N = 28
+// Output: Perfect Number
+// If sum of all proper divisors = number → Perfect Number.
+function isPerfectNumber(n) {
+  debugger;
+  if (n <= 1) return false;
+
+  let sum = 1; 
+
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      sum += i;
+
+      if (i !== n / i) {
+        sum += n / i;
+      }
+    }
+  }
+
+  return sum === n;
+}
+// console.log(isPerfectNumber(28));
+
+// 9.Find the HCF and LCM of Three Numbers
+// Input: 8, 12, 16
+// Output: HCF = 4, LCM = 48
+function findThreeNumberHCF(a, b, c) {
+  return HCF(HCF(a, b), c);
+}
+console.log(findThreeNumberHCF(8,12,16));
+
+function findThreeNumberLCM(a, b, c) {
+  return findLCM(findLCM(a, b), c);
+}
+console.log(findThreeNumberLCM(8,12,16));
---
+++ Day_06/script.js
@@ -0,0 +1,173 @@
+// 1.Check if a Number is Prime
+// Input: 7
+// Output: Prime Number
+function isPrimeNumber(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(isPrimeNumber(11));
+
+// 2. Check if two Numbers are Co-Prime
+// Input: (7, 8)
+// Output: Co-Prime
+function isCoPrime(num1, num2) {
+  let factors1 = [];
+  let greatestCF;
+  for (let i = 1; i <= num1; i++) {
+    if (num1 % i === 0) {
+      factors1.push(i);
+    }
+  }
+
+  for (let i = factors1.length; i >= 0; i--) {
+    if (num2 % factors1[i] === 0) {
+      greatestCF = factors1[i];
+      break;
+    }
+  }
+  return greatestCF === 1;
+}
+// console.log(isCoPrime(6,9))
+
+// 3.Print Fibonacci Series up to N Terms
+// Input: N = 10
+// Output: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+function fibonacciSeries(num) {
+  let x = 0;
+  let y = 1;
+  let results = [];
+  for (let i = 0; i < num; i++) {
+    results.push(x);
+    let next = x + y;
+    x = y;
+    y = next;
+  }
+  return results;
+}
+// console.log(fibonacciSeries(5));
+
+// 4. Find the Nth Fibonacci Number
+// Input: N = 8
+// Output: 13
+
+function nthFibonacciSeries(num) {
+  let results = [0n, 1n];
+  for (let i = 2n; i < BigInt(num); i++) {
+    results.push(results[i - 1n] + results[i - 2n]);
+  }
+  return results[results.length - 1];
+}
+// console.log(nthFibonacciSeries(9));
+
+// 5. Check if a Number Belongs to the Fibonacci Series
+// Input: 21
+// Output: true
+
+// using loop
+// function belogsToFibonacciSeries(num){
+//     num=BigInt(num);
+//     let results = [0n,1n];
+//     for(let i = 2n;true; i++){
+//         let nextNumber = results[i-1n]+results[i-2n];
+//         if(nextNumber === num || nextNumber === 0n) return true;
+//         if(nextNumber > num) break;
+//         results.push(nextNumber);
+//     }
+//     return false;
+// }
+// using Math Formula (5 * n^2 + 4) or (5 * n^2 - 4)
+function belogsToFibonacciSeries(num) {
+  return (
+    Number.isInteger(Math.sqrt(5 * num ** 2 + 4)) ||
+    Number.isInteger(Math.sqrt(5 * num ** 2 - 4))
+  );
+}
+
+// console.log(belogsToFibonacciSeries(8))
+
+// 6.Print All Prime Numbers up to N
+// Input: N = 20
+// Output: [2, 3, 5, 7, 11, 13, 17, 19]
+
+function printAllPrimeNumbersUptoN(num) {
+  let primeNumbers = [];
+  for (let i = 2; i <= num; i++) {
+    if (isPrimeNumber(i)) {
+      primeNumbers.push(i);
+    }
+  }
+  return primeNumbers;
+}
+// console.log(printAllPrimeNumbersUptoN(7));
+
+// 7.Sum of All Prime Numbers till N
+// Input: N = 10
+// Output: Sum = 17
+function sumOfAllPrimeNumbersUptN(num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrimeNumber(i)) {
+      sum = sum + i;
+    }
+  }
+
+  return sum;
+}
+// console.log(sumOfAllPrimeNumbersUptN(7));
+
+// 8.Check if the Sum of Two Consecutive Fibonacci Numbers is Prime
+// Input: (5 + 8)
+// Output: 13 is Prime
+
+// 9.Print First N Prime Fibonacci Numbers
+// Input: N = 5
+// Output: 2, 3, 5, 13, 89
+function printFirstNPrimeFibonacciNumbers(n) {
+  let count = 0;
+  let a = 0,
+    b = 1;
+  let result = [];
+
+  while (count < n) {
+    let fib = a + b;
+    a = b;
+    b = fib;
+
+    if (isPrimeNumber(fib)) {
+      result.push(fib);
+      count++;
+    }
+  }
+
+  return result;
+}
+// console.log(printFirstNPrimeFibonacciNumbers(5));
+
+// 11.Print All Fibonacci Numbers up to a Given Limit
+// Input: Limit = 100
+// Output: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+function printFibonacciNumberUptoN(num) {
+  let a = 0;
+  let b = 1;
+  let series = [];
+  for (let i = 2; i < num; i++) {
+    let fib = a + b;
+    if (fib > num) {
+      break;
+    }
+    a = b;
+    b = fib;
+
+    console.log(fib);
+    series.push(fib);
+  }
+  return series;
+}
+
+console.log(printFibonacciNumberUptoN(20));
